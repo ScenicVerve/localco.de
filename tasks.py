919d887bbc5ea7a2f7a36ec2ab561fac09a18efa
eleannap <eleannapan@gmail.com>
2015-06-17 15:53:23 -0700

    celery tasks

--- tasks.py
+++ tasks.py
@@ -1,7 +1,35 @@
 from celery import Celery
+from webfinches.models import *
+
+from webfinches.views import *
+#, run_once
+import topology.my_graph as mg
+import topology.my_graph_helpers as mgh
 
 app = Celery('tasks', broker='amqp://guest@localhost//')
 
+"""
+rewrite topology, using linestring list as input
+"""
 @app.task
-def add(x, y):
-    return x + y
+def run_topology(lst, name=None):
+
+    blocklist = new_import(lst,name)
+    
+    g = blocklist[0]
+
+    ep_geojson = g.myedges_geoJSON()
+    myjs = json.loads(ep_geojson)
+    print myjs
+    #map_roads = run_once(blocklist)
+    return myjs
+
+
+'''
+@app.task
+def add(x,y):
+    s = x + y
+    return s
+
+#add.delay(1,2)
+'''
