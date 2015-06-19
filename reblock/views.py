324ddc45768858f4517b339f0cf1d9e7604b9df2
panthersuper <p.panthersuper@gmail.com>
2015-06-19 16:08:06 -0700

    pull

--- reblock/views.py
+++ reblock/views.py
@@ -133,7 +133,7 @@ def compute(request):
 
     else:
         # We are browsing data
-        test_layers = PostLayerG.objects.filter(author=user).order_by('-date_edited')
+        #test_layers = PostLayerG.objects.filter(author=user).order_by('-date_edited')
         test_layers = TopoSaveJSON.objects.filter(author=user).order_by('-date_edited').filter(kind='output')
         print test_layers.all()
     c = {
@@ -194,33 +194,30 @@ def checkGeometryType(gdal_layer, srs=None):
 """
 rewrite topology, using linestring list as input, save data to the database
 """
-'''
+
 def run_topology(lst, name=None, user = None, scale_factor=1):
 
     blocklist = new_import(lst,name,scale = scale_factor)#make the graph based on input geometry
     print blocklist
     
     for i,g in enumerate(blocklist):
-        js = {}
         #ALL THE PARCELS
-        js['all'] = json.loads(g.myedges_geoJSON())
-	print js
+        parcels = json.loads(g.myedges_geoJSON())
         
         #THE INTERIOR PARCELS
         inGragh = mgh.graphFromMyFaces(g.interior_parcels)
-        js['interior'] = json.loads(inGragh.myedges_geoJSON())
+        in_parcels = json.loads(inGragh.myedges_geoJSON())
         
         #THE ROADS GENERATED and save generating process into the database
-        js['road'] = run_once(g,name = name,user = user)#calculate the roads to connect interior parcels, can extract steps
+        road = run_once(g,name = name,user = user)#calculate the roads to connect interior parcels, can extract steps
         
         lst.append(js)
         
         #save the output into the database
         lst_json = json.dumps(js)
-	print lst_json
         db_json = TopoSaveJSON(name=name, topo_json = lst_json, author = user,index = i, kind = "output")
         db_json.save()
-'''
+
 
 """
 rewrite run_once function from topology, using linestring list as input
